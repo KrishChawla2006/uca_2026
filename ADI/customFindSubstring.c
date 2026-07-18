4379d3befb1897d01722b92a250ef571790afb7e
Krish Chawla <bhuminchawla@gmail.com>
2026-07-18 19:23:31 +0530

    write Custome Sustring Find
    Corrected comment grammar and updated printf statement to display index.

---
+++ ADI/customFindSubstring.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int find_substring(const char *haystack, const char *needle) {
+    int i, j;
+
+    // If needle is empty, return 0
+    if (needle[0] == '\0')
+        return 0;
+
+    // Traverse the haystack
+    for (i = 0; haystack[i] != '\0'; i++) {
+
+        // Compare characters of needle
+        for (j = 0; needle[j] != '\0'; j++) {
+
+            if (haystack[i + j] != needle[j]) {
+                break;
+            }
+        }
+
+        // If we reach the end of needle, it matched
+        if (needle[j] == '\0') {
+            return i;
+        }
+    }
+
+    // Not found
+    return -1;
+}
+
+int main() {
+    char haystack[] = "Embedded Systems";
+    char needle[] = "bed";
+
+    int index = find_substring(haystack, needle);
+
+    if (index != -1)
+        printf(index);
+    else
+        printf("Substring not found\n");
+
+    return 0;
+}
+/*
+Output: 2
+*/
