84f607a57b70d2a866ecf2f336aab56627b3a65f
Krish Chawla <bhuminchawla@gmail.com>
2026-07-18 23:19:14 +0530

    Refactor awk tasks for improved clarity and structure

--- ADI/Practice AWK/awk_tasks.sh
+++ ADI/Practice AWK/awk_tasks.sh
@@ -1,36 +1,30 @@
 #!/bin/bash
 
-echo "Task 1: Names and Departments"
-awk -F',' 'NR>1 {print $1, $2}' employees.txt
+echo "========== Task 1 =========="
+awk -F',' '{print $1, $2}' employees.txt
 
+echo
+echo "========== Task 2 =========="
+awk -F',' '$2=="Engineering"' employees.txt
 
-echo ""
-echo "Task 2: Engineering Employees"
-awk -F',' 'NR>1 && $2=="Engineering" {print}' employees.txt
+echo
+echo "========== Task 3 =========="
+awk -F',' '{print $1 " gross pay: $" $3*$4}' employees.txt
 
+echo
+echo "========== Task 4 =========="
+awk -F',' '{print NR " (" NF " fields): " $0}' employees.txt
 
-echo ""
-echo "Task 3: Gross Pay Calculation"
-awk -F',' 'NR>1 {print $1 " gross pay: $" $3*$4}' employees.txt
-
-
-echo ""
-echo "Task 4: Line Number and Number of Fields"
-awk -F',' 'NR>1 {print (NR-1) " (" NF " fields): " $0}' employees.txt
-
-
-echo ""
-echo "Task 5: Total Engineering Payroll"
-
+echo
+echo "========== Task 5 =========="
 awk -F',' '
 BEGIN {
-    total=0
+    total = 0
 }
-NR>1 {
-	if($2=="Engineering") {
-		total += $3*$4
-	}
+$2 == "Engineering" {
+    total += $3 * $4
 }
 END {
-	print "Total Engineering Payroll: $" total
-} ' employees.txt
+    print "Total Engineering Payroll: $" total
+}
+' employees.txt
