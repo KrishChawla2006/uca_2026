a3b6720c4678a6440b8c48ebf70e5411ab2d9574
Krish Chawla <bhuminchawla@gmail.com>
2026-07-19 10:17:34 +0530

    Add assignment question comment to tokenizer.c
    Added a comment block with the assignment question for clarity.

--- ADI/Week2 Assignment/tokenizer.c
+++ ADI/Week2 Assignment/tokenizer.c
@@ -1,3 +1,8 @@
+/*
+Question:
+25. The Tokenizer
+Write a C code snippet using strtok() that takes the string "C,C++,Java,Python,Rust" and prints each language on a new line. Please submit your solution by providing a GitHub file link.
+*/
 #include <stdio.h>
 #include <string.h>
 
