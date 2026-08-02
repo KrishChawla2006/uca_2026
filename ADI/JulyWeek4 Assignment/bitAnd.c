b0079bfa967aa3371812e29f07b98f521739eea2
Krish Chawla <bhuminchawla@gmail.com>
2026-08-02 15:36:41 +0530

    file added

---
+++ ADI/JulyWeek4 Assignment/bitAnd.c
@@ -0,0 +1,32 @@
+/*
+Question:
+bitAnd
+Implement following function with the given constraints:
+
+/* 
+ * bitAnd - x&y using only ~ and | 
+ *   Example: bitAnd(6, 5) = 4
+ *   Legal ops: ~ |
+ *   Max ops: 8
+ *   Rating: 1
+ */
+int bitAnd(int x, int y) {
+  return 0;
+}
+*/
+#include<stdio.h>
+
+int bitAnd(int x, int y) {
+  return ~(~ x | ~ y);
+}
+
+int main(){
+  int x;
+  int y;
+  printf("Enter the value of x:");
+  scanf("%d",&x);
+  printf("Enter the value of y:");
+  scanf("%d",&y);
+  printf("%d & %d = %d",x,y,bitAnd(x,y));
+  return 0;
+}
